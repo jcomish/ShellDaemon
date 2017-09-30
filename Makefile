57fdf34c29d78b0a6b7ce0bad0c3b5db0fbafd7a
Joshua Comish <jomish2323@gmail.com>
2017-09-30 01:59:02 -0500

    Implement pthreads

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: yash.c input.c process.c
 	gcc -g -Wall -o yash yash.c input.c
-	gcc -g -Wall -o yashd yashd.c input.c process.c
+	gcc -g -Wall -o yashd yashd.c input.c process.c -pthread
 
 yashd.o : yashd.c input.h process.h
-	cc -c yashd.c
+	cc -c yashd.c -pthread
 
 yash.o : yash.c input.h process.h
 	cc -c yash.c
--- yashd.c
+++ yashd.c
@@ -9,14 +9,29 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <string.h>
+
+#include <pthread.h>
 
 #include "input.h"
 #include "process.h"
 
-void clearBuffer(char * buf);
+#define NUM_THREADS 25
 
+typedef struct _thread_data_t {
+  int tid;
+  int stuff;
+} thread_data_t;
+
+
+static volatile int count=0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_t thr[NUM_THREADS];
+thread_data_t thr_data[NUM_THREADS];
+int threadsIndex = 0;
 int sd, psd;
+
+void clearBuffer(char * buf);
 
 void reusePort(int s)
 {
@@ -29,35 +44,51 @@ void reusePort(int s)
 	}
 }   
 
-void setupSocket()
-{
-    struct   sockaddr_in server;
-    sd = socket (AF_INET,SOCK_STREAM,0);
-    
-    
-    
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = 3826;
-    
-    reusePort(sd);
-    if ( bind( sd, (struct sockaddr *) &server, sizeof(server) ) < 0 ) {
-	close(sd);
-	perror("binding name to stream socket");
-	exit(-1);
-        }
-    
-    listen(sd,1);
-    psd = accept(sd, 0, 0);
-    
+/* thread function Ex1: thread_data_t Input; no output*/
+void *processThread(void *arg) {
     char buf[1024];
+    int cc;
+    
+    printf("Thread: %d\n", threadsIndex);
     
     clearBuffer(buf);
     recv(psd,buf,sizeof(buf), 0);
     
     printf("'%s'\n", buf);
     send(psd, "#", 3, 0 );
+
+    for(;;) 
+    {
+        bool ISDEBUG = true;
+        clearBuffer(buf);
+        cc=recv(psd,buf,sizeof(buf), 0);
+            if (cc == 0) return;
+        buf[cc] = '\0';
+        printf("message received: %s", buf);
+       
+        //char * results = handleRawInput(buf);
+       
+        send(psd, "Command Recieved", 16, 0);
+        send(psd, "\n#", 3, 0 );
+       
+    }
     
+    return;
+}
+
+void spawnThread()
+{
+    int i, rc;
+    
+    thr_data[threadsIndex].tid = threadsIndex;
+    if ((rc = pthread_create(&thr[threadsIndex], NULL, processThread, &thr_data[threadsIndex]))) {
+      fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
+      return;
+    }
+    //pthread_join(thr[threadsIndex], NULL);
+
+
+    return;
 }
 
 void clearBuffer(char * buf)
@@ -69,35 +100,42 @@ void clearBuffer(char * buf)
     }
 }
 
+void setupSocket()
+{
+    struct sockaddr_in server;
+    sd = socket (AF_INET,SOCK_STREAM,0);
+    
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = 3826;
+    
+    reusePort(sd);
+        if (bind( sd, (struct sockaddr *) &server, sizeof(server)) < 0 ) 
+        {
+            close(sd);
+            perror("binding name to stream socket");
+            exit(-1);
+        }
+}
+
 int main(int argc, char** argv) 
 {
-   char   buf[1024];
-   int    cc;
+    setupSocket();
+    
+    for (;;)
+    {
+        listen(sd,1);
+        psd = accept(sd, 0, 0);
+//thr_data[threadsIndex] = accept(sd, 0, 0);
+        spawnThread();
+        threadsIndex++;
+        
+        printf("Socket Established\n");
+    }
 
-   setupSocket();
-   
-   for(;;) 
-   {
-       bool ISDEBUG = true;
-       clearBuffer(buf);
-       cc=recv(psd,buf,sizeof(buf), 0);
-           if (cc == 0) exit (0);
-       buf[cc] = '\0';
-       printf("message received: %s", buf);
-       char **stringList;
-       stringList = splitInput(buf, ISDEBUG);
-       char ***commandList = splitIntoCommands(stringList, ISDEBUG);
-       
-       printCommands(commandList);
-       
-       char *results;
-       results = malloc(sizeof(char) * 1024);
-       results = "Command Results";
-       
-       send(psd, results, 15, 0);
-       send(psd, "\n#", 3, 0 );
-       
-   }
+    return 0;
+    
+
 
 }
 
